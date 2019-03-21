d0b32f3a0d62fc73c334b6009c882bf51b8a7f72
Abhishek K Das <abhishek.das.contact@gmail.com>
2019-03-21 00:41:46 -0500

    refactor(train.py): clean up the code

--- train.py
+++ train.py
@@ -1,17 +1,13 @@
 import os
 import keras
 import csv
-import matplotlib.image as mpimg
 import numpy as np
-import matplotlib.pyplot as plt
 from keras.preprocessing import image
+from keras import optimizers
 
 WIDTH = 320
 HEIGHT = 320
 num_classes = 4
-
-train_csv_path = '/floyd/input/chexpert/train.csv'
-validate_csv_path = '/floyd/input/chexpert'
 
 
 def read_data(filepath):
@@ -30,23 +26,27 @@ def read_data(filepath):
 	(column_names, data)
 		Tuple of names of the columns and the data.
 	"""
-	_data = []
-	_column_names = []
+	data = []
+	column_names = []
 
 	with open(filepath, 'rt') as csvfile:
 		data_reader = csv.reader(csvfile, delimiter=',')
 		flag = False
 		for row in data_reader:
 			if not flag:
-				_column_names = row
+				column_names = row
 				flag = True
 			else:
-				_data.append(row)
+				data.append(row)
 
-	return _column_names, _data
+	return column_names, np.array(data)
 
 
-column_names, data = read_data(train_csv_path)
+train_csv_path = '/floyd/input/chexpert/train.csv'
+column_names, train_data = read_data(train_csv_path)
+
+validate_csv_path = '/floyd/input/chexpert/valid.csv'
+_, validate_data = read_data(validate_csv_path)
 
 
 def preprocess_y(raw_y):
@@ -56,8 +56,18 @@ def preprocess_y(raw_y):
 	return y
 
 
+# cardiomegaly train and output
+y_train = preprocess_y(train_data[:, 7])
+y_validate = preprocess_y(validate_data[:, 7])
+
+
+# Referencing the following blog to generate the data
+# https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+#
+
 class DataGenerator(keras.utils.Sequence):
 	'Generates data for Keras'
+
 	def __init__(self,
 				 list_IDs,
 				 labels,
@@ -114,7 +124,7 @@ class DataGenerator(keras.utils.Sequence):
 			img_path = img_path.replace('home/CheXpert-v1.0-small', 'input/chexpert')
 			img = image.load_img(img_path, target_size=(WIDTH, HEIGHT), color_mode='grayscale')
 
-			X[i, ] = image.img_to_array(img) / 255
+			X[i,] = image.img_to_array(img) / 255
 
 			# store the class
 			y[i] = self.labels[ID]
@@ -122,16 +132,9 @@ class DataGenerator(keras.utils.Sequence):
 		return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
 
 
-params = {
-	'dim': (320, 320),
-	'batch_size': 32,
-	'n_classes': 4,
-	'n_channels': 1,
-	'shuffle': True
-}
-
 
 def get_train_and_validation(train_ids, y_train, validation_ids, y_validation):
+	'Get the train and validation ids and label'
 	partition = {}
 	partition['train'] = train_ids
 	partition['validation'] = validation_ids
@@ -146,8 +149,50 @@ def get_train_and_validation(train_ids, y_train, validation_ids, y_validation):
 	return partition, labels_
 
 
-if __name__ == '__main__':
-	print('Hello world! And bye')
+partition, labels = get_train_and_validation(train_data[:, 0], y_train, validate_data[:, 0], y_validate)
+
+params = {
+    'dim': (320, 320),
+    'batch_size': 16,
+    'n_classes': 4,
+    'n_channels': 1,
+    'shuffle': True
+}
+
+training_generator = DataGenerator(partition['train'], labels, **params)
+validation_generator = DataGenerator(partition['validation'], labels, **params)
 
 
+model = keras.applications.densenet.DenseNet121(
+                                        include_top=True,
+                                        weights=None,
+                                        input_shape=(320, 320, 1),
+                                        pooling='max',
+                                        classes=4)
 
+callbacks_list = [
+    keras.callbacks.EarlyStopping(
+        monitor='acc',
+        patience=1,
+    ),
+    keras.callbacks.ModelCheckpoint(
+        filepath='chexpert_weights.h5',
+        monitor='val_loss',
+        save_best_only=True,
+    )
+]
+
+optimizer = optimizers.Adam(lr=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+
+model.compile(loss='categorical_crossentropy',
+			  optimizer=optimizer,
+			  metrics=["accuracy"])
+
+model.fit_generator(
+	train_generator,
+	steps_per_epoch=13963,
+	callbacks=callbacks_list,
+	epochs=10,
+	validation_data=validation_generator,
+	validation_steps=14
+)
